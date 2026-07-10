7d3372e10cce5e03673037cf5ced492e9c01b789
a20240676-crypto <a20240676@pucp.edu.pe>
2026-07-10 17:42:24 -0500

    Update PC3.py

--- PC3.py
+++ PC3.py
@@ -5,7 +5,13 @@
 # python -m venv .venv
 # Esto nos permite crear un entorno virtual donde instalaremos Streamlit 
 # y observaremos la página web que se está generando en este script.
-from streamlit_option_menu import option_menu
+import streamlit as st
+# Herramienta para crear menús de navegación personalizados en Streamlit.
+from streamlit_option_menu import option_menu  # <--- ¡AÑADE ESTA LÍNEA!
+
+# Este módulo permite incrustar componentes personalizados escritos en HTML, CSS y JavaScript dentro de una aplicación.
+# components.html() permite mostrar código HTML interactivo directamente en la interfaz.
+import streamlit.components.v1 as components
 # Opcional: Activaremos el entorno virtual.
 # En Windows:
 # .venv\Scripts\activate
